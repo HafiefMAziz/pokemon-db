c8945b563909b7deb5057e8f3786894c68f11539
Hafief Maulana Aziz <Hafief.Maulana.Aziz@hashmicro.com>
2024-02-12 15:19:04 +0700

    done search pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState , useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import SearchBar from "./components/SearchBar/SearchBar";
+import PokemonCard from './components/PokemonCard/PokemonCard';
 
 function App() {
+  const pokemonState = useSelector((state) => state.pokemon);
+  const pokemonData = pokemonState.pokemon;
+  const error = pokemonState.error;
+  console.log(error);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar/>
+      <div>
+        {error && <p>{error}</p>}
+        {pokemonData && <PokemonCard pokemonData={pokemonData} />}
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPokemon } from "../../redux/pokemonSlice";
+import axios from "axios";
+import "./styles.css";
+
+function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const dispatch = useDispatch();
+  
+  const searchPokemon = async () => {
+    dispatch(fetchPokemon(searchTerm));
+  };
+
+  return (
+    <>
+      <div className="search-container">
+        <h1>Pokemon Search</h1>
+        <input
+          type="text"
+          id="searchInput"
+          placeholder="Search Pokemon..."
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}>Search</button>
+      </div>
+    </>
+  );
+}
+
+export default SearchBar;
